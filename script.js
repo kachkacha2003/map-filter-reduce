d3701d7564484cd8fb9addc845e1ae7950e5768d
kachkacha2003 <lasha.katchkatchishvili.1@btu.edu.ge>
2023-10-23 22:02:46 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// let arr=[1,2,3,4,5,6];
+// let filtered=arr.filter((number)=>number%2==0);
+// console.log(filtered);
+
+
+// let arr=[
+//     {
+//         name:"lasha",
+//         age:20
+//     },
+//     {
+//         name:"nika",
+//         age:17
+        
+//     },
+//     {
+//         name:"irakli",
+//         age:18
+//     },
+//     {
+//         name:"irali",
+//         age:18
+//     },
+//     {
+//         name:"irkli",
+//         age:18
+//     }
+    
+
+// ]
+
+// let filtered=arr.filter((baqari)=>baqari.age>=18);
+// filtered.map((person)=>console.log(person.age))
+// let array=[1,2,3,4,5];
+// let maped2=array.map((item)=>item *2);
+// console.log(maped2)
+// console.log(filtered)
+// let filtered=arr.filter((baqari)=>baqari.age>=18);
+// let maped=filtered.map((person)=>console.log(person));
+// console.log(maped)
+
+// let arr2=[1,2,3,4,5,6];
+// //reduce
+// let result=arr2.reduce((a,b)=>a+b);
+// console.log(result/arr2.length)
